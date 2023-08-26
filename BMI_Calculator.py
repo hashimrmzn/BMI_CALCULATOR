a461c51de8e34e3dbd7e1351f16a3a899af841d5
hashimrmzn <hashimramzan246@gmail.com>
2023-08-27 02:37:59 +0500

    here i started my bmi calculator using python

---
+++ BMI_Calculator.py
@@ -0,0 +1,13 @@
+
+
+
+name= str(input("Please enter your name: "))
+
+print("Now you are going to Calculate Your Bmi")
+
+print( name + "  you have to give me  yours's body informations like   ")
+
+while True:
+
+  height = float(input("Enter your height in cm: "))
+  weight = float(input("Enter your weight in kg: "))
